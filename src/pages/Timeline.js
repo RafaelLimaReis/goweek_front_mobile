1b5f07ccee2c0340a46326ad633d1fb23b66407b
RafaelLimaReis <rafaellimareis01@gmail.com>
2018-11-09 16:11:19 -0200

    Initial commit

---
+++ src/pages/Timeline.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+
+import { View, Text, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import api from '../services/api';
+import socket from 'socket.io-client';
+import Tweet from '../components/Tweet';
+
+export default class Timeline extends Component {
+  static navigationOptions = ({navigation}) => ({
+    title: 'Inicio',
+    headerRight: (
+      <TouchableOpacity onPress={() => navigation.navigate('New')}>
+        <Icon 
+          name="add-circle-outline"
+          size={24}
+          style={{ marginRight: 20 }}
+          color="#4BB0EE"  
+        />
+      </TouchableOpacity>
+    )
+  });
+
+  state = {
+    tweets: []
+  }
+  
+  async componentDidMount() {
+    this.subscribeToEvent();
+    const response = await api.get('tweets');
+
+    this.setState({ tweets: response.data});
+  }
+
+  subscribeToEvent() {
+    const io = socket('http://192.168.1.15:3000');
+
+    io.on('tweet', data => {
+      this.setState({ tweets: [data, ...this.state.tweets] });
+    });
+    io.on('like', data => {
+      this.setState({
+        tweets: this.state.tweets.map(
+          tweet => (tweet._id === data._id ? data : tweet)
+        )
+      });
+    });
+  }
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={this.state.tweets}
+          keyExtractor={tweet => tweet._id}
+          renderItem={({ item }) => <Tweet tweet={item}/>} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFF"
+  }
+});
